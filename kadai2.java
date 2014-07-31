2ddd110fc1e7f8765c77739acdd43e33c377f6d8
rou <rou@voyager-inc.com>
2014-07-31 18:27:07 +0900

    Java研修 - 課題2： 7/31

--- kadai2.java
+++ kadai2.java
@@ -1,44 +1,32 @@
 package abc;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class kadai2 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//[Integer.parseInt(br.readLine())][Integer.parseInt(br.readLine())] ; 
-		System.out.println("数値を入力してください");
-
-		String hen = br.readLine();
-		String shu = br.readLine();
-		
-		int a = hen.length();
-		int b = shu.length();
-		
-		int arr[][] = new int[a][a]; 
+	public static void main(String[] args) { 
+		//配列　arr　を定義する
+		int arr[][] = new int[][] {{ 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }}; 
 		System.out.println("変更前：");
+		//（printArrayメソッドを使って）配列　arr　を表示する
 			printArray(arr);
+		//配列　arr2 を定義する
 			int arr2[][] = new int[arr.length][arr.length];
-			for (int i = 0; i < arr.length; i++) 
-			{
-				for (int j = 0; j < arr[i].length; j++) 
-				{
+			for (int i = 0; i < arr.length; i++){
+				for (int j = 0; j < arr[i].length; j++) {
 					arr2[i][j] = arr[j][i];
 				}
 			}
+
 				System.out.println("変更後：");
+		//（printArrayメソッドを使って）配列　arr2　を表示する
 			printArray(arr2);
 		}
-		private static void printArray(int[][] arr) 
-		{
-			for (int i = 1; i < arr.length; i++) 
-			{
-				for (int j = 1; j < arr.length; j++) 
-				{
+		//printArrayメソッドの作成
+		private static void printArray(int[][] arr) {
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = 0; j < arr.length; j++) {
 					System.out.print(arr[i][j] + " ");
 				}
-			System.out.println();
+				System.out.println();
 			}
 		}
-}
+	}
